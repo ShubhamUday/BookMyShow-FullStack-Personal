324420a4ddd6409f1034f939cf55cc60a0e42a13
shubhamuday <shubhamuday21@gmail.com>
2025-01-27 11:08:48 +0530

    first commit

---
+++ bookmyshow-project/server/models/upcomingModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const upcomingSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+    releaseDate: {
+      type: Date,
+      required: true,
+    },
+    poster: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+
+const Upcoming = mongoose.model("upcoming", upcomingSchema);
+
+module.exports = Upcoming
